d2d86d135c070d2592df83330cd3329076a46842
Jacky Chen (SPE) (from Dev Box) <qichen@microsoft.com>
2024-09-20 11:16:21 +0800

    test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require('express')();
 const PORT = 4399;
 
+app.get('/', (req, res) => {
+    res.send("Hello World!!!");
+});
+
 app.get('/test', (req, res) => {
     res.status(200).send({
         mytestdata: 'this is test'
