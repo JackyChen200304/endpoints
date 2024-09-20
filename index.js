f811f935a89097428a3f7ea7cbc4f0ce0148243a
Jacky Chen (SPE) (from Dev Box) <qichen@microsoft.com>
2024-09-20 09:56:07 +0800

    add hackathon

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const PORT = 8080;
+
+app.listen(
+    PORT,
+    () => console.log('running')
+)
+
+app.get('/keyword/summarize', (req, res) => {
+    const fs = require('fs')
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file:', err);
+            res.status(400).send();
+            return;
+        }
+        try {
+            jsonData = JSON.parse(data);
+            console.log('JSON data:', jsonData);
+            res.status(200).send(jsonData);
+        } catch (err) {
+            console.error('Error parsing JSON:', err);
+            res.status(400).send();
+        }
+    });
+});
