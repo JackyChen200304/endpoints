3cf808fbd11daa5d64f180e0e912a804745a8708
Jacky Chen (SPE) (from Dev Box) <qichen@microsoft.com>
2024-09-22 10:07:09 +0800

    add ports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const app = require('express')();
-const PORT = 4399;
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello World!!!");
